cbf2376eb3d7f453684209e35ba892230a198d1c
BENAZIR\riphah 1 <benazir.ashraf07@gmail.com>
2017-06-05 13:52:29 +0500

    dropdown updated and fix the event location

--- library.php
+++ library.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$_SESSION["language"] = "en"; // set the default language of the website
+$_SESSION["language"] = "english"; // set the default language of the website
 if(isset($_GET["lang"]) && $_GET["lang"] != ""){ // check if get any language change parameter and change the session value
 	$_SESSION["language"] = $_GET["lang"];
 }
